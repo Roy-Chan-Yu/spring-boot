d3fbf12cd5c1c51d223de69c164373252e24c53b
roy.chan <w983874@gmail.com>
2021-07-13 22:10:52 +0800

    i18n for RESTful web-service

--- src/main/java/com/pouchen/roy/springboot/Application.java
+++ src/main/java/com/pouchen/roy/springboot/Application.java
@@ -1,7 +1,13 @@
 package com.pouchen.roy.springboot;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +15,22 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.CHINESE);
+		
+		return localeResolver;
+	}
+	
+	public ResourceBundleMessageSource messageSource() {
+		
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("i18n");
+		
+		return messageSource;
+	}
+	
 }
--- src/main/java/com/pouchen/roy/springboot/controller/StudentController.java
+++ src/main/java/com/pouchen/roy/springboot/controller/StudentController.java
@@ -1,15 +1,18 @@
 package com.pouchen.roy.springboot.controller;
 
 import java.util.List;
+import java.util.Locale;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pouchen.roy.springboot.exception.StudentNotFoundException;
@@ -21,6 +24,9 @@ public class StudentController {
 	
 	@Autowired
 	StudentService studentService;
+	
+	@Autowired
+	private MessageSource messageSource;  
 	
 	@GetMapping("/students")
 	public List<Student> getAllStudents() {
@@ -37,6 +43,16 @@ public class StudentController {
 		}
 		return student;
 		
+	}
+	
+	@GetMapping("students/{id}/internationalized")
+	public Student studentInternationalized(@PathVariable("id") int id ,@RequestHeader(name = "Accept-Language", required = false) Locale locale ) {
+		
+		Student student = new Student();
+		student.setId(155);
+		student.setName(messageSource.getMessage("good.morning.message", null,locale));
+		
+		return student;
 	}
 	
 	@PostMapping("/student")
